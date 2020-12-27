6284d328d01312229ac891694f8a599690e3e200
Fire Monkey <sik.pascal@gmail.com>
2020-12-27 18:40:49 +0100

    final stuff

--- server.js
+++ server.js
@@ -10,10 +10,18 @@ mongoose.connect(mongoConnectionString, { useNewUrlParser: true, useUnifiedTopol
   .then(succ => console.log('Connected to mongodb: ' + succ))
   .catch(fail => console.log('Failed to connect to mongodb: ' + fail));
 
+const exerciseSchema = new mongoose.Schema({
+  description: String,
+  duration: Number,
+  date: Date
+})
+
 const userSchema = new mongoose.Schema({
-  username: { type: String, unique: true, required: true }
+  username: { type: String, unique: true, required: true },
+  exercises: [exerciseSchema]
 });
 const UserModel = mongoose.model('User', userSchema);
+
 
 app.use(cors())
 app.use(express.static('public'))
@@ -49,7 +57,71 @@ app.post("/api/exercise/new-user", async (req, res) => {
 });
 
 app.get('/api/exercise/users', async (req, res) => {
-  res.json(await UserModel.find());
+  res.json(await UserModel.find().select('_id username'));
+});
+
+app.post('/api/exercise/add', async (req, res) => {
+  console.log('Received new exercise: ' + JSON.stringify(req.body));
+  const {
+    userId,
+    description,
+    duration,
+    date
+  } = req.body;
+
+  const user = await UserModel.findById(userId);
+  if (!user) {
+    res.send('Unknown user');
+    return;
+  };
+
+  const exDate = new Date(date || new Date().toDateString());
+  user.exercises.push({ description, duration, date: exDate });
+
+  user.save()
+    .then(doc => {
+      console.log('Added new exercise: ' + doc);
+      res.json({
+        username: doc.username,
+        description,
+        duration: parseInt(duration),
+        _id: doc._id,
+        date: exDate.toDateString(),
+      });
+    })
+    .catch(error => {
+      console.log('Failed to add exercise: ' + error);
+      res.status(e.status).json({ error: e.message });
+    });
+});
+
+app.get('/api/exercise/log', (req, res) => {
+  const {
+    userId,
+    from,
+    to,
+    limit
+  } = req.query;
+
+  UserModel.findById(userId)
+    .then(doc => {
+      let log = [];
+      log = doc.exercises
+        .filter(l => l.date >= new Date(from || -8640000000000000) && l.date <= new Date(to || 8640000000000000))
+        .slice(0, limit || doc.exercises.length)
+        .map(l => ({
+          duration: l.duration,
+          description: l.description,
+          date: l.date.toDateString()
+        }));
+      res.status(200).json({
+        _id: doc._id,
+        username: doc.username,
+        count: doc.exercises.length,
+        log
+      })
+    })
+    .catch(e => res.json({ error: e }));
 });
 
 const listener = app.listen(process.env.PORT || 3000, () => {
